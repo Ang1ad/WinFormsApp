b7a0b538689718014cd78c608c967b5421321793
Anglad <ya.anton-glladkov02@yandex.ru>
2024-04-26 17:52:28 +0300

    Added Form2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,30 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
+            loginLoginTextBox = new TextBox();
             button1 = new Button();
             button2 = new Button();
-            textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
-            textBox5 = new TextBox();
+            passwordLoginTextBox = new TextBox();
+            loginRegisterTextBox = new TextBox();
+            passwordRegisterTextBox = new TextBox();
+            repeatPasswordRegisterTextBox = new TextBox();
+            loginLabelLogin = new Label();
+            passwordLabelLogin = new Label();
+            loginLabelRegister = new Label();
+            passwordRegister = new Label();
+            repeatPasswordRegister = new Label();
             SuspendLayout();
             // 
-            // textBox1
+            // loginLoginTextBox
             // 
-            textBox1.Location = new Point(12, 79);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 0;
+            loginLoginTextBox.Location = new Point(28, 83);
+            loginLoginTextBox.Name = "loginLoginTextBox";
+            loginLoginTextBox.Size = new Size(125, 27);
+            loginLoginTextBox.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(31, 193);
+            button1.Location = new Point(47, 193);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 1;
             button1.Text = "Login";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -62,46 +68,99 @@
             button2.Text = "Register";
             button2.UseVisualStyleBackColor = true;
             // 
-            // textBox2
+            // passwordLoginTextBox
             // 
-            textBox2.Location = new Point(12, 141);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 3;
+            passwordLoginTextBox.Location = new Point(28, 145);
+            passwordLoginTextBox.Name = "passwordLoginTextBox";
+            passwordLoginTextBox.Size = new Size(125, 27);
+            passwordLoginTextBox.TabIndex = 3;
+            passwordLoginTextBox.UseSystemPasswordChar = true;
             // 
-            // textBox3
+            // loginRegisterTextBox
             // 
-            textBox3.Location = new Point(215, 16);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(125, 27);
-            textBox3.TabIndex = 4;
+            loginRegisterTextBox.Location = new Point(215, 35);
+            loginRegisterTextBox.Name = "loginRegisterTextBox";
+            loginRegisterTextBox.Size = new Size(125, 27);
+            loginRegisterTextBox.TabIndex = 4;
             // 
-            // textBox4
+            // passwordRegisterTextBox
             // 
-            textBox4.Location = new Point(215, 79);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(125, 27);
-            textBox4.TabIndex = 5;
+            passwordRegisterTextBox.Location = new Point(215, 98);
+            passwordRegisterTextBox.Name = "passwordRegisterTextBox";
+            passwordRegisterTextBox.Size = new Size(125, 27);
+            passwordRegisterTextBox.TabIndex = 5;
+            passwordRegisterTextBox.UseSystemPasswordChar = true;
             // 
-            // textBox5
+            // repeatPasswordRegisterTextBox
             // 
-            textBox5.Location = new Point(215, 141);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(125, 27);
-            textBox5.TabIndex = 6;
+            repeatPasswordRegisterTextBox.Location = new Point(215, 160);
+            repeatPasswordRegisterTextBox.Name = "repeatPasswordRegisterTextBox";
+            repeatPasswordRegisterTextBox.Size = new Size(125, 27);
+            repeatPasswordRegisterTextBox.TabIndex = 6;
+            repeatPasswordRegisterTextBox.UseSystemPasswordChar = true;
+            // 
+            // loginLabelLogin
+            // 
+            loginLabelLogin.AutoSize = true;
+            loginLabelLogin.Location = new Point(71, 60);
+            loginLabelLogin.Name = "loginLabelLogin";
+            loginLabelLogin.Size = new Size(46, 20);
+            loginLabelLogin.TabIndex = 7;
+            loginLabelLogin.Text = "Login";
+            // 
+            // passwordLabelLogin
+            // 
+            passwordLabelLogin.AutoSize = true;
+            passwordLabelLogin.Location = new Point(59, 122);
+            passwordLabelLogin.Name = "passwordLabelLogin";
+            passwordLabelLogin.Size = new Size(70, 20);
+            passwordLabelLogin.TabIndex = 8;
+            passwordLabelLogin.Text = "Password";
+            // 
+            // loginLabelRegister
+            // 
+            loginLabelRegister.AutoSize = true;
+            loginLabelRegister.Location = new Point(254, 12);
+            loginLabelRegister.Name = "loginLabelRegister";
+            loginLabelRegister.Size = new Size(46, 20);
+            loginLabelRegister.TabIndex = 9;
+            loginLabelRegister.Text = "Login";
+            // 
+            // passwordRegister
+            // 
+            passwordRegister.AutoSize = true;
+            passwordRegister.Location = new Point(242, 75);
+            passwordRegister.Name = "passwordRegister";
+            passwordRegister.Size = new Size(70, 20);
+            passwordRegister.TabIndex = 10;
+            passwordRegister.Text = "Password";
+            // 
+            // repeatPasswordRegister
+            // 
+            repeatPasswordRegister.AutoSize = true;
+            repeatPasswordRegister.Location = new Point(217, 137);
+            repeatPasswordRegister.Name = "repeatPasswordRegister";
+            repeatPasswordRegister.Size = new Size(123, 20);
+            repeatPasswordRegister.TabIndex = 11;
+            repeatPasswordRegister.Text = "Repeat password";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(370, 252);
-            Controls.Add(textBox5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(textBox2);
+            Controls.Add(repeatPasswordRegister);
+            Controls.Add(passwordRegister);
+            Controls.Add(loginLabelRegister);
+            Controls.Add(passwordLabelLogin);
+            Controls.Add(loginLabelLogin);
+            Controls.Add(repeatPasswordRegisterTextBox);
+            Controls.Add(passwordRegisterTextBox);
+            Controls.Add(loginRegisterTextBox);
+            Controls.Add(passwordLoginTextBox);
             Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(textBox1);
+            Controls.Add(loginLoginTextBox);
             Name = "Form1";
             Text = "Login or register";
             ResumeLayout(false);
@@ -110,12 +169,17 @@
 
         #endregion
 
-        private TextBox textBox1;
+        private TextBox loginLoginTextBox;
         private Button button1;
         private Button button2;
-        private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
-        private TextBox textBox5;
+        private TextBox passwordLoginTextBox;
+        private TextBox loginRegisterTextBox;
+        private TextBox passwordRegisterTextBox;
+        private TextBox repeatPasswordRegisterTextBox;
+        private Label loginLabelLogin;
+        private Label passwordLabelLogin;
+        private Label loginLabelRegister;
+        private Label passwordRegister;
+        private Label repeatPasswordRegister;
     }
 }
